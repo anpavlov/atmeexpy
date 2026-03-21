75dcbcf654bc659b021b4258582b8ae5dc02407d
Andrey Pavlov <andrey.pavlov@corp.mail.ru>
2026-03-21 03:33:32 +0300

    Allow external http_client injection in AtmeexClient

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='atmeexpy',
-    version='0.1.4',
+    version='0.2.0',
     url='https://github.com/anpavlov/atmeexpy',
     author='Andrey Pavlov',
     author_email='dir94@mail.ru',
